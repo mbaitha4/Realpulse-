45668cc8c2755e99227565fbe7ca7cbbc830597a
mbaitha4 <mbaitha4@gmail.com>
2026-02-18 16:19:27 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,26 +1,3 @@
-import clientPromise from "../lib/mongodb.js";
-
-export default async function handler(req, res) {
-  try {
-
-    if (!process.env.MONGODB_URI) {
-      return res.status(500).json({ error: "MONGODB_URI missing" });
-    }
-
-    const client = await clientPromise;
-    const db = client.db("realpulse");
-
-    const articles = await db.collection("articles")
-      .find({})
-      .limit(5)
-      .toArray();
-
-    return res.status(200).json({ articles });
-
-  } catch (error) {
-    return res.status(500).json({
-      message: "Server crashed",
-      error: error.message
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({ status: "working" });
 }
