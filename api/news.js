f25d01ac8991230c5e55396e0441bf012caa787e
mbaitha4 <mbaitha4@gmail.com>
2026-02-18 16:40:37 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,11 +1,7 @@
-import clientPromise from "../lib/mongodb.js";
+const clientPromise = require("../lib/mongodb");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
-    if (!process.env.MONGODB_URI) {
-      return res.status(500).json({ error: "MONGODB_URI not set" });
-    }
-
     const client = await clientPromise;
     const db = client.db("realpulse");
 
@@ -14,11 +10,9 @@ export default async function handler(req, res) {
       .limit(5)
       .toArray();
 
-    return res.status(200).json({ success: true, count: articles.length });
+    res.status(200).json({ success: true, count: articles.length });
 
   } catch (err) {
-    return res.status(500).json({
-      error: err.message
-    });
+    res.status(500).json({ error: err.message });
   }
-}
+};
