904f4b4d5d36755a1b863887697fb2e5d594ee96
mbaitha4 <mbaitha4@gmail.com>
2026-02-18 12:52:44 +0530

    Create fetch.js

---
+++ api/fetch.js
@@ -0,0 +1,34 @@
+import clientPromise from "../lib/mongodb";
+
+export default async function handler(req,res){
+
+const API_KEY = process.env.GNEWS_API_KEY;
+const client = await clientPromise;
+const db = client.db("realpulse");
+
+const categories=["general","nation","business","technology","sports","miscellaneous"];
+const langs=["en","hi"];
+
+for(const lang of langs){
+for(const category of categories){
+
+const response=await fetch(
+`https://gnews.io/api/v4/top-headlines?category=${category}&lang=${lang}&country=in&max=10&apikey=${API_KEY}`
+);
+
+const data=await response.json();
+
+if(data.articles){
+for(const article of data.articles){
+await db.collection("articles").updateOne(
+{ url: article.url },
+{ $set: { ...article, category, lang } },
+{ upsert:true }
+);
+}
+}
+}
+}
+
+res.status(200).json({message:"Fetched"});
+}
