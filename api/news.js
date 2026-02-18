b21758e53d44e50e37636e16b38363d06aea206e
mbaitha4 <mbaitha4@gmail.com>
2026-02-18 17:05:24 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,18 +1,18 @@
-const clientPromise = require("../lib/mongodb");
+const connectToDatabase = require("../lib/mongodb");
 
 module.exports = async function handler(req, res) {
   try {
-    const client = await clientPromise;
-    const db = client.db("realpulse");
+    const { db } = await connectToDatabase();
 
-    const articles = await db.collection("articles")
+    const articles = await db
+      .collection("articles")
       .find({})
       .limit(5)
       .toArray();
 
     res.status(200).json({ success: true, count: articles.length });
 
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
